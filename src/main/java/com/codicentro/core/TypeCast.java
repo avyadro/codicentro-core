cf597defa3ef757ae33c1556b79cfff8516790b0
alexandervy <alexandervy@ce6ecb83-be4f-5c5c-d187-ccf6864a73a3>
2012-05-16 13:18:28 +0000

    Update...

--- src/main/java/com/codicentro/core/TypeCast.java
+++ src/main/java/com/codicentro/core/TypeCast.java
@@ -924,6 +924,36 @@ public class TypeCast {
         return new FileInputStream(file);
     }
 
+    public static byte[] toBytes(File file) throws IOException {
+        ByteArrayOutputStream ous = null;
+        InputStream ios = null;
+        try {
+            byte[] buffer = new byte[4096];
+            ous = new ByteArrayOutputStream();
+            ios = new FileInputStream(file);
+            int read = 0;
+            while ((read = ios.read(buffer)) != -1) {
+                ous.write(buffer, 0, read);
+            }
+        } finally {
+            try {
+                if (ous != null) {
+                    ous.close();
+                }
+            } catch (IOException e) {
+                // swallow, since not that important
+            }
+            try {
+                if (ios != null) {
+                    ios.close();
+                }
+            } catch (IOException e) {
+                // swallow, since not that important
+            }
+        }
+        return ous.toByteArray();
+    }
+
     /**
      * Gets the exception stack trace as a string.
      *
