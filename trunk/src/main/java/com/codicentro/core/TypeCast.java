fe13378395470dbc2f4d6f53f81a075312b88ee3
hgr.santiago <hgr.santiago@ce6ecb83-be4f-5c5c-d187-ccf6864a73a3>
2013-02-22 23:09:25 +0000

    toBytes from InputStream

--- trunk/src/main/java/com/codicentro/core/TypeCast.java
+++ trunk/src/main/java/com/codicentro/core/TypeCast.java
@@ -1114,4 +1114,25 @@ public class TypeCast {
             return TypeCast.toString(value);
         }
     }
+
+    public static byte[] toBytes(InputStream is) throws IOException {
+
+        int len;
+        int size = 1024;
+        byte[] buf;
+
+        if (is instanceof ByteArrayInputStream) {
+            size = is.available();
+            buf = new byte[size];
+            len = is.read(buf, 0, size);
+        } else {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            buf = new byte[size];
+            while ((len = is.read(buf, 0, size)) != -1) {
+                bos.write(buf, 0, len);
+            }
+            buf = bos.toByteArray();
+        }
+        return buf;
+    }
 }
